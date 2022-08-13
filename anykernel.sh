d784f27da052f796c89fe2a3bc8d612aa5828e5b
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-08-13 10:09:30 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy Kernel by shashank1439
+kernel.string=Phoenix Kernel by phoenix1708
 do.devicecheck=1
 do.modules=0
 do.systemless=1
